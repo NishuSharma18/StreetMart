8bc3a7f2a98df4e8b4f253476adcfa8b484fcdff
NishuSharma18 <as.nishu18@gmail.com>
2022-04-24 08:58:45 +0530

    some more information has been added

--- app/src/main/java/com/example/streetmart/secondActivity.java
+++ app/src/main/java/com/example/streetmart/secondActivity.java
@@ -1,17 +1,26 @@
 package com.example.streetmart;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import java.util.Objects;
 
 public class secondActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+
+    String s1[],s2[];
+    int images[] ={R.drawable.honey,R.drawable.brush,R.drawable.tomato,R.drawable.oximeter,R.drawable.apple,R.drawable.tshirt,R.drawable.shoes,R.drawable.bp};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        recyclerView = findViewById(R.id.recyclerView);
         Objects.requireNonNull(getSupportActionBar()).hide();
+        s1 = getResources().getStringArray(R.array.items);
     }
 }
